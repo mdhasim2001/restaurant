b6c15557e766202bcb31c935f63c35be7c644d07
mdhasim2001 <mdhasim.nana.2001@gmail.com>
2024-07-08 22:22:07 +0530

    banner ok

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -1,51 +1,63 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"
+import "./Navbar.css";
 import { MdRestaurantMenu } from "react-icons/md";
+import logo from "../../assets/home/logo.png";
 
 const Navbar = () => {
+  const [navbar, setNavbar] = useState(true);
 
-    const [navbar, setNavbar] = useState(true)
+  const navbarTule = (nav) => {
+    setNavbar(nav);
+  };
 
-    const navbarTule = (nav) => {
-        setNavbar(nav)
-    }
-
-    console.log(navbar);
+  console.log(navbar);
 
   return (
-    <div className=" flex items-center justify-between lg:w-4/5 lg:mx-auto mx-5 h-20 text-[14px]">
-      <Link to="/" className="text-xl uppercase cursor-pointer">
-        <img src="../../assets/logo.png" alt="" />
-        Restaurant
+    <div className="lg:w-[90%] lg:mx-auto flex items-center justify-between  mx-5 h-20 text-[14px]">
+      <Link to="/" className="text-xl uppercase cursor-pointer ">
+        <img className="w-10" src={logo} alt="" />
       </Link>
       <div className="flex items-center justify-center">
-            <div className={`${navbar ? "hidden" : "hasim"}  lg:block`}>
-                <ul className="uppercase lg:flex lg:gap-5">
-                    <button onClick={()=>navbarTule(true)} className="right-5 top-5 absolute lg:hidden"><MdRestaurantMenu className="w-5 h-5" /></button>
-                    <li onClick={() => navbarTule(true)}>
-                    <Link>Home</Link>
-                    </li>
-                    <li onClick={() => navbarTule(true)}>
-                    <Link>Contact Us</Link>
-                    </li>
-                    <li onClick={() => navbarTule(true)}>
-                    <Link to="dashboard">DASHBOARD</Link>
-                    </li>
-                    <li onClick={() => navbarTule(true)}>
-                    <Link>our menu</Link>
-                    </li>
-                    <li onClick={() => navbarTule(true)}>
-                    <Link>our shop</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="lg:hidden">
-                {
-                    navbar ? <button onClick={() => navbarTule(false)} className="w-10 h-3"><MdRestaurantMenu className="w-5 h-5" /></button> : 
-                    ""
-                }
-            </div>
+        <div className={`${navbar ? "hidden" : "hasim"}  lg:block`}>
+          <ul className="uppercase lg:flex lg:gap-5">
+            <button
+              onClick={() => navbarTule(true)}
+              className="right-5 top-5 absolute lg:hidden"
+            >
+              <MdRestaurantMenu className="w-5 h-5" />
+            </button>
+            <li onClick={() => navbarTule(true)}>
+              <Link>Home</Link>
+            </li>
+            <li onClick={() => navbarTule(true)}>
+              <Link>Contact Us</Link>
+            </li>
+            <li onClick={() => navbarTule(true)}>
+              <Link to="dashboard">DASHBOARD</Link>
+            </li>
+            <li onClick={() => navbarTule(true)}>
+              <Link>our menu</Link>
+            </li>
+            <li onClick={() => navbarTule(true)}>
+              <Link>our shop</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex items-center justify-center gap-5 h-full">
+        <div>
+          <Link className="uppercase cursor-pointer">sing up</Link>
+        </div>
+        <div className="lg:hidden">
+          {navbar ? (
+            <button onClick={() => navbarTule(false)}>
+              <MdRestaurantMenu className="w-5 h-5" />
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ src/Pages/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import "./Banner.css"
+
+const Banner = () => {
+    return (
+        <div className="banner flex flex-col items-center justify-center">
+            <p className="tracking-[1px]">(2024)</p>
+            <h1 className="text-7xl tracking-[3px] my-5">A SWEET LUXURY</h1>
+        </div>
+    );
+};
+
+export default Banner;
